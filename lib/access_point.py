56315f43c4736432453012649e07d720cc01cc28
Monazir Muhammad Doha <mmdoha@houndsec.net>
2026-04-11 15:14:31 +0600

    feat: add open accesss point support

--- lib/access_point.py
+++ lib/access_point.py
@@ -2,7 +2,7 @@ import network
 
 
 class AccessPoint:
-    def __init__(self, ssid: str, password: str):
+    def __init__(self, ssid: str, password: str = ""):
         self.ssid = ssid
         self.password = password
         self.ap = network.WLAN(network.AP_IF)
@@ -11,11 +11,31 @@ class AccessPoint:
         if not self.ap.active():
             self.ap.active(True)
 
-        self.ap.config(essid=self.ssid, password=self.password, authmode=3)
+        # Decide mode based on password
+        if self.password:
+            # Secured network (WPA2)
+            self.ap.config(
+                essid=self.ssid,
+                password=self.password,
+                authmode=3
+            )
+        else:
+            # Open network
+            self.ap.config(
+                essid=self.ssid,
+                authmode=0
+            )
 
         ip = self.ap.ifconfig()[0]
+
         print("AP started")
         print("SSID:", self.ssid)
+        print("Mode:", "OPEN" if not self.password else "WPA2")
         print("IP:", ip)
 
         return ip
+
+    def stop(self):
+        if self.ap.active():
+            self.ap.active(False)
+            print("AP stopped")
